0ee14169fe0872045276e8250b070788d1330d3b
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-06-09 17:57:09 +0530

    Update C1 Teacher's copy.py

--- C1 Teacher's copy.py
+++ C1 Teacher's copy.py
@@ -6,7 +6,6 @@ screen = pygame.display.set_mode((1200, 400))
 
 dino_rect = pygame.Rect(100, 250, 64, 64)
 cactus_rect = pygame.Rect(1100, 300, 32, 32)
-ground_rect = pygame.Rect(0, 330, 1200, 2)
 
 while True:
     screen.fill((255, 255, 255))
@@ -17,7 +16,6 @@ while True:
 
     pygame.draw.rect(screen, (100, 100, 100), dino_rect)
     pygame.draw.rect(screen, (100, 100, 100), cactus_rect)
-    pygame.draw.rect(screen, (100, 100, 100), ground_rect)
     
     pygame.display.update()
     
